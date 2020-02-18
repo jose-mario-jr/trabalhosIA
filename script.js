c4af0bea4861111ac880e526f328f51d2a27590e
ze@home <jmsj777@gmail.com>
2020-02-17 21:09:56 -0300

    mutacao, cruzamento prontos, apenas fazer torneio -> otimizar alguns deles! parece nao estar funcionando bem

--- script.js
+++ script.js
@@ -36,11 +36,70 @@ function fitness(cromossome) {
   )
 }
 
-function roulette(popOld) {
-  let sumFit = 0
-  let acumulatorProb = 0
-  let arrayRoleta = []
-  let selected = []
+function sorteio(arrayRoleta) {
+  let rand = Math.random()
+  let c = 0
+  while (arrayRoleta[c] <= rand) {
+    c++
+  }
+  return c
+}
+
+function operadorCruzamento(popOld, probCruzamento) {
+  let crossed = []
+  for (let i = 0; i < popOld.length / 2; i++) {
+    sorteados = [
+      Math.floor(Math.random() * popOld.length),
+      Math.floor(Math.random() * popOld.length)
+    ]
+    while (sorteados[0] == sorteados[1])
+      sorteados[1] = Math.floor(Math.random() * popOld.length)
+    casal = [popOld[sorteados[0]], popOld[sorteados[1]]]
+    console.log(casal, sorteados)
+    cruzaOuNao = Math.random()
+    if (cruzaOuNao < probCruzamento) {
+      pontoCruzamento = Math.round(Math.random() * casal[0].genes.length)
+      cromossomosNovos = [{ genes: [] }, { genes: [] }]
+      for (var index = 0; index < pontoCruzamento; index++) {
+        cromossomosNovos[0].genes[index] = casal[0].genes[index]
+        cromossomosNovos[1].genes[index] = casal[1].genes[index]
+      }
+      for (
+        var index = pontoCruzamento;
+        index < casal[0].genes.length;
+        index++
+      ) {
+        cromossomosNovos[1].genes[index] = casal[1].genes[index]
+        cromossomosNovos[0].genes[index] = casal[0].genes[index]
+      }
+      crossed.push(...cromossomosNovos)
+    } else {
+      // só passa para frente
+      crossed.push(...casal)
+    }
+  }
+  return crossed
+}
+
+function operadorMutacao(popOld, probMutacao) {
+  let muted = popOld
+  console.log(muted)
+  for (let i = 0; i < muted.length; i++) {
+    for (let j = 0; j < muted[i].genes.length; j++) {
+      mutaOuNao = Math.random()
+      if (mutaOuNao < probMutacao) {
+        muted[i].genes[j] = Math.round(Math.random())
+      }
+    }
+  }
+  return muted
+}
+
+function roulette(popOld, probCruzamento, probMutacao) {
+  var sumFit = 0
+  var acumulatorProb = 0
+  var arrayRoleta = []
+  var selected = []
 
   for (let c = 0; c < popOld.length; c++) {
     sumFit += popOld[c].fitness
@@ -51,42 +110,60 @@ function roulette(popOld) {
     acumulatorProb = popOld[c].probNow + acumulatorProb
     arrayRoleta.push(acumulatorProb)
   }
-  console.log(popOld, arrayRoleta)
   for (let i = 0; i < popOld.length; i++) {
-    let rand = Math.random()
-    let c = 0
-    while (arrayRoleta[c] <= rand) {
-      c++
-    }
-    selected.push(popOld[c])
+    let pos = sorteio(arrayRoleta)
+
+    selected.push(popOld[pos])
   }
 
-  return selected
+  let crossed = operadorCruzamento(selected, probCruzamento)
+  let muted = operadorMutacao(crossed, probMutacao)
+  return muted
 }
 
 function botaoClicado() {
-  let tamanhoC = document.getElementById("tamanhoC").value
-  let tamanhoPop = document.getElementById("tamanhoPop").value
-  let probCruzamento = document.getElementById("probCruzamento").value
-  let probMutacao = document.getElementById("probMutacao").value
-  let qtGeracoes = document.getElementById("qtGeracoes").value
-  let tipo = document.getElementById("tipo").value
+  const tamanhoC = document.getElementById("tamanhoC").value
+  const tamanhoPop = document.getElementById("tamanhoPop").value
+  const probCruzamento = document.getElementById("probCruzamento").value
+  const probMutacao = document.getElementById("probMutacao").value
+  const qtGeracoes = document.getElementById("qtGeracoes").value
+  const tipo = document.getElementById("tipo").value
+  console.log(probCruzamento, probMutacao)
 
   var population = initialPopulation(tamanhoPop, tamanhoC)
   // calcula aptidao
   for (let k = 0; k < population.length; k++) {
     population[k].fitness = parseFloat(fitness(population[k]).toFixed(2))
   }
-  for (let cont = 0; cont < qtGeracoes; cont++) {
-    // seleciona melhores por roleta
-    let best = roulette(population)
-    population = best
-  }
-
   var add = ""
+
+  if (tipo == 1) {
+    // roleta
+    for (let cont = 0; cont < qtGeracoes; cont++) {
+      // seleciona melhores por roleta
+      var best = roulette(population, probCruzamento, probMutacao)
+      population = best
+
+      // calcula aptidao
+      for (let k = 0; k < population.length; k++) {
+        population[k].fitness = parseFloat(fitness(population[k]).toFixed(2))
+      }
+
+      add += `Geracao ${cont}, individuos: `
+      for (k = 0; k < population.length; k++) {
+        add += `
+          <span title="${population[k].genes}">
+            ${population[k].fitness};   
+          </span>  `
+      }
+      add += `<br />`
+    }
+  }
+  add += `populacao resultante: <br />`
   for (k = 0; k < population.length; k++) {
-    console.log(population)
-    add += `${population[k].fitness} -> ${population[k].genes}<br/>`
+    add += `${population[k].genes} -> ${population[k].fitness} <br/>`
   }
   document.getElementById("log").innerHTML = add
 }
+
+//--------------------------------------------------------
