2a39c2c7c9402928afede7c0adb8936e1a00456f
José Mário <jmsj777@hotmail.com>
2020-03-06 18:00:08 +0000

    tabela na index de trab23

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
-<?php include_once("index.html"); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Trabalhos de IA</title>
+    <style>
+      table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+      }
+      
+      td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+      }
+      
+      tr:nth-child(even) {
+        background-color: #dddddd;
+      }
+      </style>
+  </head>
+  <body>
+    <h1>Trabahlos IA - Professor José Ricardo</h1>
+
+    <a href="trab1">Ir para Trab1</a>
+    <a href="trab2">Ir para Trab2</a>
+
+    
+  </body>
+</html>
