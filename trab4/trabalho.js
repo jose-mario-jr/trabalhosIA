1df0c21947b35e8d76ac6d5d24bd44721570cd18
ze@home <jmsj777@gmail.com>
2020-03-24 16:01:05 -0300

    trabalho 60% -> falta plotar grafico de grafos e corrigir bugs

--- trab4/trabalho.js
+++ trab4/trabalho.js
@@ -1,125 +1,79 @@
-function shuffle(a) {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
-
-var myArray = ["1", "2", "3", "4", "5"];
-console.log(shuffle(myArray));
+//a tabela é assim, linhas e colunas: 
+// Uberaba,Uberlândia,Araxá,Patos de Minas,Patrocínio,Monte Carmelo,Araguari,Ituiutaba,Prata,Frutal,Conceição das Alagoas,Campo Florido,Perdizes,Santa Juliana,Nova Ponte,Delta,Água Comprida,Sacramento,Conquista,Comendador Gomes
+const tabelaDistancias = [
+	[0  ,108,117,251,180,150,141,245,149,131,65 ,73 ,126,100,76 ,34 ,47 ,80 ,61 ,138],
+	[108,0  ,177,221,150,108,39 ,137,87 ,182,166,174,135,110,74 ,141,148,158,168,150],
+	[117,177,0  ,161,107,185,210,313,263,251,178,187,56 ,91 ,110,142,160,86 ,116,252],
+	[251,221,161,0  ,75 ,153,219,357,312,405,332,341,139,188,180,296,314,251,272,375],
+	[180,150,107,75 ,0  ,82 ,148,286,241,328,256,264,63 ,111,109,220,238,151,172,304],
+	[150,108,185,153,82 ,0  ,100,245,199,290,218,227,140,118,72 ,182,200,165,186,262],
+	[141,39 ,210,219,148,100,0  ,171,125,220,198,206,167,142,116,173,180,218,200,188],
+	[245,137,313,357,286,245,171,0  ,103,199,209,175,270,245,209,277,284,293,304,151],
+	[149,87 ,263,312,241,199,125,103,0  ,101,111,77 ,225,200,164,177,184,222,204,69 ],
+	[131,182,251,405,328,290,220,199,101,0  ,75 ,76 ,266,227,216,165,122,210,192,52 ],
+	[65 ,166,178,332,256,218,198,209,111,75 ,0  ,35 ,193,168,144,93 ,50 ,141,120,100],
+	[73 ,174,187,341,264,227,206,175,77 ,76 ,35 ,0  ,202,177,152,101,108,150,128,66 ],
+	[126,135,56 ,139,63 ,140,167,270,225,266,193,202,0  ,49 ,68 ,158,175,89 ,110,267],
+	[100,110,91 ,188,111,118,142,245,200,227,168,177,49 ,0  ,43 ,119,137,72 ,93 ,229],
+	[76 ,74 ,110,180,109,72 ,116,209,164,216,144,152,68 ,43 ,0  ,107,125,91 ,112,217],
+	[34 ,141,142,296,220,182,173,277,177,165,93 ,101,158,119,107,0  ,74 ,51 ,33 ,166],
+	[47 ,148,160,314,238,200,180,284,184,122,50 ,108,175,137,125,74 ,0  ,123,101,170],
+	[80 ,158,86 ,251,151,165,218,293,222,210,141,150,89 ,72 ,91 ,51 ,123,0  ,24 ,211],
+	[61 ,168,116,272,172,186,200,304,204,192,120,128,110,93 ,112,33 ,101,24 ,0  ,193],
+	[138,150,252,375,304,262,188,151,69 ,52 ,100,66 ,267,229,217,166,170,211,193,0  ],
+]
 
 //gera populacao inicial
-function populacaoInicial(individuos = 10, genes = 21) {
+function populacaoInicial(individuos = 10, genes = 20) {
   var pop = [];
   for (i = 0; i < individuos; i++) {
     var cromossomo = {
       genes: [0]
     };
-    // popula cada cromossomo com 21 genes de 0 a 4
+    // popula cada cromossomo com 20 genes de 1 a 19
     for (j = 1; j < genes; j++) {
 	  do {
-	  	var escolhido = Math.floor(Math.random() * 20 + 1) // escolhe de 1 a 20
+	  	var escolhido = Math.floor(Math.random() * (genes-1) + 1) // escolhe de 1 a 20
 	  } while(cromossomo.genes.includes(escolhido))
       cromossomo.genes.push(escolhido);
     }
 	cromossomo.genes.push(0)
-	console.log(cromossomo)
+
     pop.push(cromossomo);
   }
+
   return pop;
 }
 
 // faz a funcão de aptidao que calcula a aptidao de certo individuo
-function aptidao(genes, curso0 = false, curso2 = false) {
+function aptidao(genes) {
   //aplicar regras de aptidao aqui!!
-  var aptidao = 0;
-  var serveOuNao = true;
-  // regra 2: Cada profissional deve folgar 3 turnos antes de poder voltar ao trabalho
-  for (let z = 0; z < genes.length; z++) {
-    for (let w = 1; w < 4; w++) {
-      if (genes[z] == genes[w + z]) {
-        aptidao -= 10;
-        serveOuNao = false;
-      } else if (z < genes.length) aptidao++;
-    }
-  }
+  var aptidao = 5000;
 
-  // regra 3: fazer checagem de curso para 1 e 2 e aptidão para este caso de folga de 6 turnos
-  if (curso0) {
-    for (let z = 0; z < genes.length; z++) {
-      // o gene 0 corresponde a equipe de 1 e 2
-      if (genes[z] == 0) {
-        let curso0Valido = true;
-        for (let w = 1; w < 7; w++) {
-          if (genes[z] == genes[w + z]) curso0Valido = false;
-        }
-        if (curso0Valido) {
-          aptidao += 10;
-          z = genes.length;
-        } else {
-          serveOuNao = false;
-        }
-      }
-    }
+  for (let z = 0; z < genes.length-1; z++) {
+    aptidao -= tabelaDistancias[genes[z]][genes[z+1]]
   }
-
-  // regra 4: fazer checagem de curso para 5 e 6 e aptidão para este caso de folga de 5 turnos
-  if (curso2) {
-    for (let z = 0; z < genes.length; z++) {
-      // o gene 2 corresponde a equipe de 5 e 6
-      if (genes[z] == 2) {
-        let curso2Valido = true;
-        for (let w = 1; w < 6; w++) {
-          if (genes[z] == genes[w + z]) curso2Valido = false;
-        }
-        if (curso2Valido) {
-          aptidao += 10;
-          z = genes.length;
-        } else {
-          serveOuNao = false;
-        }
-      }
-    }
-  }
-
-  // regra 5: diferenca entre quantidade de turnos deve ser menor possivel
-  var turnosEquipe = [0, 0, 0, 0, 0];
-  var turnosValidos = true;
-  for (let z = 0; z < genes.length; z++) {
-    turnosEquipe[genes[z]]++;
-  }
-  for (p = 0; p < turnosEquipe.length; p++) {
-    if (turnosEquipe[p] != 5 && turnosEquipe[p] != 4) {
-      turnosValidos = false;
-    }
-  }
-  return {
-    valor: aptidao,
-    valido: serveOuNao,
-    turnosValidos: turnosValidos
-  };
+  return aptidao;
 }
 
-function operadorCruzamento(casal, probCruzamento, doisPontos = false) {
+function operadorCruzamento(casal, probCruzamento) {
   var filhos = [{ genes: [] }, { genes: [] }];
 
   var cruzaOuNao = Math.random();
   if (cruzaOuNao < probCruzamento) {
-    var ponto1 = Math.round(Math.random() * casal[0].genes.length);
-    var ponto2 = casal[0].genes.length;
-    if (doisPontos) {
-      var ponto2 = Math.round(Math.random() * casal[0].genes.length);
-      while (ponto1 == ponto2)
-        ponto2 = Math.round(Math.random() * casal[0].genes.length);
+	
+	// cruzamento pmx
+    var ponto1 = Math.floor(Math.random() * (casal[0].genes.length-2)+1);
+    var ponto2 = Math.round(Math.random() * (casal[0].genes.length-2)+1);
+    while (ponto1 == ponto2)
+      ponto2 = Math.round(Math.random() * (casal[0].genes.length-2)+1);
 
-      if (ponto1 > ponto2) {
-        let aux = ponto2;
-        ponto2 = ponto1;
-        ponto1 = aux;
-      }
+    if (ponto1 > ponto2) {
+      let aux = ponto2;
+      ponto2 = ponto1;
+      ponto1 = aux;
     }
-
+    
     // console.log("Ocorreu Cruzamento, corte = " + ponto1)
     for (var index = 0; index < ponto1; index++) {
       filhos[1].genes.push(casal[1].genes[index]);
@@ -129,12 +83,22 @@ function operadorCruzamento(casal, probCruzamento, doisPontos = false) {
       filhos[1].genes.push(casal[0].genes[index]);
       filhos[0].genes.push(casal[1].genes[index]);
     }
-    if (doisPontos) {
-      for (var index = ponto2; index < casal[0].genes.length; index++) {
-        filhos[1].genes.push(casal[1].genes[index]);
-        filhos[0].genes.push(casal[0].genes[index]);
-      }
+    for (var index = ponto2; index < casal[0].genes.length; index++) {
+      filhos[1].genes.push(casal[1].genes[index]);
+      filhos[0].genes.push(casal[0].genes[index]);
     }
+
+	for(let i = 1; i<filhos[0].genes.length-1; i++){
+	  for(let j = i; j<filhos[0].genes.length-1; j++){
+		if(filhos[0].genes[i] == filhos[0].genes[j]){ // se entra aqui, quer dizer que repetiu algum
+		  for(let k = 1; k<filhos[0].genes.length-1; k++){
+			if(!filhos[0].genes.includes(k)){ // quer dizer que este é o que foi excluido pelo cruzamento
+			  filhos[0].genes[i] = k
+			}
+		  }
+		}
+	  }
+	}
     // console.log("casal: ", casal)
     // console.log("filhos: ", filhos)
     return filhos;
@@ -145,18 +109,22 @@ function operadorCruzamento(casal, probCruzamento, doisPontos = false) {
 
 function operadorMutacao(popOld, probMutacao) {
   let mutado = popOld;
-  for (let i = 0; i < mutado.length; i++) {
-    for (let j = 0; j < mutado[i].genes.length; j++) {
+  for (let i = 0; i < mutado.length; i++) { // para cada individuo
+    for (let j = 1; j < mutado[i].genes.length - 1; j++) { // para cada gene
       let mutaOuNao = Math.random();
       if (mutaOuNao < probMutacao) {
-        mutado[i].genes[j] = Math.floor(Math.random() * 5);
+
+		indMutar = Math.floor(Math.random() * 19 + 1)
+		let aux = mutado[i].genes[j]
+		mutado[i].genes[j] = mutado[i].genes[indMutar]
+		mutado[i].genes[indMutar] = aux
       }
     }
   }
   return mutado;
 }
 
-function roleta(popOld, probCruzamento, probMutacao, doisPontos, elite) {
+function roleta(popOld, probCruzamento, probMutacao, elite) {
   var sumFit = 0;
   var acumulatorProb = 0;
   var arrayRoleta = [];
@@ -193,7 +161,7 @@ function roleta(popOld, probCruzamento, probMutacao, doisPontos, elite) {
     let casal = [selected[sorteados[0]], selected[sorteados[1]]];
 
     var tam = filhos.push(
-      ...operadorCruzamento(casal, probCruzamento, doisPontos)
+      ...operadorCruzamento(casal, probCruzamento)
     );
 
     while (tam > tamanhoRetorno) {
@@ -231,7 +199,7 @@ function melhorCasal(individuos, lista) {
   return [maior, segundoMaior];
 }
 
-function torneio(popOld, tamanhoTorneio, probCruzamento, doisPontos, elite) {
+function torneio(popOld, tamanhoTorneio, probCruzamento, elite) {
   var filhos = [];
   var tamanhoRetorno = popOld.length - elite.length;
 
@@ -247,7 +215,7 @@ function torneio(popOld, tamanhoTorneio, probCruzamento, doisPontos, elite) {
     //selecionar casal
     var casal = melhorCasal(popOld, sorteados);
 
-    tam = filhos.push(...operadorCruzamento(casal, probCruzamento, doisPontos));
+    tam = filhos.push(...operadorCruzamento(casal, probCruzamento));
     while (tam > tamanhoRetorno) {
       filhos.pop();
       tam--;
@@ -314,26 +282,11 @@ function botaoClicado() {
   var population = populacaoInicial(tamanhoPop);
   var geracao = 0;
 
-  var acumuladorApt = [];
-  var horarioPerfeito = null;
-
   // calcula aptidao
   for (let k = 0; k < population.length; k++) {
-    let apt = aptidao(population[k].genes, curso0, curso2);
-    population[k].aptidao = apt.valor;
-    population[k].valido = apt.valido;
-    population[k].turnosValidos = apt.turnosValidos;
-    if (population[k].valido) {
-      acumuladorApt.push(getData(population, 0));
-      //atualizaTabela(population[k])
-      // alert(`Achou um individuo apto!
-      //   Aptidao: ${population[k].aptidao},
-      //   geração encontrada = 0`)
-      if (population[k].turnosValidos) {
-        horarioPerfeito = population[k];
-      }
-    }
+    population[k].aptidao = aptidao(population[k].genes);
   }
+  console.log(population)
 
   var add = `Geracao 0, individuos: `;
   for (k = 0; k < population.length; k++) {
@@ -358,7 +311,7 @@ function botaoClicado() {
 
     if (tipo == 1) {
       // seleciona melhores por roleta
-      best = roleta(population, probCruzamento, probMutacao, doisPontos, elite);
+      best = roleta(population, probCruzamento, probMutacao, elite);
     }
     if (tipo == 2) {
       // faz torneio
@@ -366,7 +319,6 @@ function botaoClicado() {
         population,
         tamanhoTorneio,
         probCruzamento,
-        doisPontos,
         elite
       );
     }
@@ -374,37 +326,15 @@ function botaoClicado() {
     geracao++;
     // calcula aptidao
     for (let k = 0; k < population.length; k++) {
-      let apt = aptidao(population[k].genes, curso0, curso2);
-      population[k].aptidao = apt.valor;
-      population[k].valido = apt.valido;
-      population[k].turnosValidos = apt.turnosValidos;
-      if (population[k].valido && population[k].turnosValidos) {
-        horarioPerfeito = population[k];
-        horarioPerfeito.geracaoEncontrado = geracao;
-        console.log("Horario perfeito: ", horarioPerfeito);
-        break;
-      }
+
+      population[k].aptidao = aptidao(population[k].genes);
+
       if (population[k].aptidao > melhorIndividuo.aptidao) {
         melhorIndividuo = population[k];
         melhorIndividuo.geracaoEncontrado = geracao;
       }
     }
-    acumuladorApt.push(getData(population, geracao));
     acumuladorMelhor.push(melhorIndividuo);
-
-    // var melhorAgora = elitismo(population, 1)[0]
-
-    // if (melhorAgora.aptidao > melhorIndividuo.aptidao ) {
-    //   if (melhorAgora.valido) {
-    //     melhorIndividuo = melhorAgora
-    //     melhorIndividuo.geracaoEncontrado = geracao
-    //     if(melhorAgora.turnosValidos){
-    //       //atualizaTabela(melhorIndividuo)
-    //       horarioPerfeito = melhorIndividuo
-
-    //     }
-    //   }
-    // }
 
     // // poe na view
     add += `Geracao ${geracao}, individuos: `;
@@ -415,28 +345,21 @@ function botaoClicado() {
           </span>  `;
     }
     add += `<br />`;
-    if (geracao > 100000) {
-      alert("chegou na geracao 100000 limite, algoritmo parado!");
-      return;
-    }
 
     if (Date.now() - tempo0 > tempoLimite) {
       alert("Chegou no tempo limite, algoritmo parado!");
       return;
     }
-  } while (!horarioPerfeito);
-  plotaAptidao(acumuladorApt);
-  plotaMelhores(acumuladorMelhor);
+  } while (geracao < geracoesLimite)
   add += `populacao resultante: <br />`;
   for (k = 0; k < population.length; k++) {
     add += `${population[k].genes} -> ${population[k].aptidao} <br/>`;
   }
   document.getElementById("log").innerHTML = add;
 
-  atualizaTabela(horarioPerfeito);
   alert(`Achado o individuo perfeito!
-    Aptidao: ${horarioPerfeito.aptidao}, 
-    Geração encontrada = ${horarioPerfeito.geracaoEncontrado}
+    Aptidao: ${melhorIndividuo.aptidao}, 
+    Geração encontrada = ${melhorIndividuo.geracaoEncontrado}
     Tempo: ${(Date.now() - tempo0) / 1000} s`);
 }
 
