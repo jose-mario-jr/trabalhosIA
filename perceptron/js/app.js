54ac5c57bbd20d7430a6b792b2a77ed6b2a60cdc
ze@home <jmsj777@hotmail.com>
2020-04-28 15:16:15 -0300

    com perceptron plotado

--- perceptron/js/app.js
+++ perceptron/js/app.js
@@ -10,6 +10,21 @@ function sumMultList(m1, m2) {
   return sum
 }
 
+function multiplyMatrices(m1, m2) {
+  let result = []
+  for (let i = 0; i < m1.length; i++) {
+    result[i] = []
+    for (let j = 0; j < m2[0].length; j++) {
+      let sum = 0
+      for (let k = 0; k < m1[0].length; k++) {
+        sum += m1[i][k] * m2[k][j]
+      }
+      result[i][j] = sum
+    }
+  }
+  return result
+}
+
 const x = [
   [1, 1],
   [1, -1],
@@ -36,8 +51,9 @@ v = v.map((v) => {
 let v0 = random() //v0
 let v0Anterior = 0
 
-test = 1
-ciclo = 0
+let log = ""
+let test = 1
+let ciclo = 0
 while (test == 1) {
   let cont = 0
 
@@ -55,11 +71,60 @@ while (test == 1) {
     v0 = v0Anterior + alfa * (t[i] - y[i])
   }
   ciclo++
-
   console.log("Ciclo: " + ciclo)
   console.log(cont)
   if (cont == amostras) test = 0
 }
 
+log += "Ciclos: " + ciclo + "<br />"
+
+log += "Vetor de pesos: " + v.reduce((acc, e) => `${acc}, ${e}`) + "<br />"
+log += "Bias final: " + v0
 console.log(v)
 console.log(v0)
+
+let teste = multiplyMatrices(x, v)
+let yfinal = Array(amostras).fill(0)
+let vetor = Array(amostras).fill(1)
+let yaux = teste.map((e, i) => {
+  return e + v0 * vetor[i]
+})
+
+y = yaux.map((e) => {
+  return e >= limiar ? 1 : -1
+})
+
+console.log(y)
+
+let vx = Array.from(new Array(200), (e, i) => (i * 4) / 200 - 2)
+let vy = Array(200).fill(0)
+
+vy = vy.map((e, i) => -(v0 + v[0] * vx[i]) / v[1])
+
+const trace1 = {
+  x: [1, 1, -1, -1],
+  y: [1, -1, 1, -1],
+  mode: "markers",
+  type: "scatter",
+  name: "Pontos de teste",
+}
+const trace2 = {
+  x: vx,
+  y: vy,
+  mode: "lines",
+  type: "scatter",
+  name: "Reta resultante",
+}
+const data = [trace1, trace2]
+
+const layout = {
+  xaxis: {
+    range: [-1.5, 1.5],
+  },
+  yaxis: {
+    range: [-1.5, 1.5],
+  },
+}
+
+document.querySelector("#log").innerHTML = log
+Plotly.newPlot("myDiv", data, layout)
